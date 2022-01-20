0fd6422813afef9363de0d505e785149f11d2cd8
Nicholas Nolen <nicnolen@ymail.com>
2022-01-20 13:54:23 -0600

    added form to search zookeepers by name or age

--- routes/htmlRoutes/index.js
+++ routes/htmlRoutes/index.js
@@ -1,26 +1,20 @@
-// Import the path and router methods
 const path = require('path');
 const router = require('express').Router();
 
-// Create a GET route for index.html
 router.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/index.html'));
+  res.sendFile(path.join(__dirname, '../../public/index.html'));
 });
 
-// Create a GET route for animals.js
 router.get('/animals', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/animals.html'));
+  res.sendFile(path.join(__dirname, '../../public/animals.html'));
 });
 
-// Create a GET route for zookeepers.js
 router.get('/zookeepers', (req, res) => {
-  res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+  res.sendFile(path.join(__dirname, '../../public/zookeepers.html'));
 });
 
-// Make a wildcard route to catch requests for routes that dont exist
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-// Export the file
 module.exports = router;
